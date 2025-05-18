59d52e52f982015c9a2ecad90d001089bb30bcc1
MuhammadUsman-10 <usman.aamir404@gmail.com>
2025-05-18 17:31:43 +0500

    Project Initiated

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
+      <div className=" py-3">
+        <div className="relative px-4 py-10  shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="">
+            <div className="divide-y divide-gray-200">
+              <div className="py-8 text-base space-y-4 text-gray-700 sm:text-lg ">
+                <h1 className="text-3xl text-white font-bold text-center mb-8">
+                  Welcome to React + Vite + Tailwind
+                </h1>
+                <p className="text-center text-white">
+                  Start building your awesome project!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
