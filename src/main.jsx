701f079570a1fd99a3b93d64ac685cba7dd9c195
MuhammadUsman-10 <usman.aamir404@gmail.com>
2025-06-16 12:41:39 +0500

    Project Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { ParallaxProvider } from 'react-scroll-parallax';
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ParallaxProvider>
+  <StrictMode>
     <App />
-  </ParallaxProvider>,
+  </StrictMode>,
 )
